e7441cf13b7c4785b2cc0aa60a95feed831470ca
Christopher Harrison <ch12@sanger.ac.uk>
2016-03-02 16:56:32 +0000

    datetime <> Unix time

--- hgijson/json/primitive.py
+++ hgijson/json/primitive.py
@@ -2,6 +2,9 @@ from json import JSONDecoder, JSONEncoder
 
 from typing import Any
 
+from datetime import datetime
+from time import mktime
+
 
 class StrJSONEncoder(JSONEncoder):
     """
@@ -47,5 +50,21 @@ class FloatJSONDecoder(JSONDecoder):
     """
     JSON decoder for floats.
     """
-    def decode(self, to_decode: str, **kwargs) -> float:
-        return float(to_decode)
+    def decode(self, to_decode: str, **kwargs) -> str:
+        return str(to_decode)
+
+
+class DatetimeEpochJSONEncoder(JSONEncoder):
+    """
+    JSON encoder for datetime
+    """
+    def default(self, to_encode: datetime) -> int:
+        return int(mktime(to_encode.timetuple()))
+
+
+class DatetimeEpochJSONDecoder(JSONDecoder):
+    """
+    JSON decoder for datetime
+    """
+    def decode(self, to_decode: str, **kwargs) -> datetime:
+        return datetime.fromtimestamp(to_decode)
