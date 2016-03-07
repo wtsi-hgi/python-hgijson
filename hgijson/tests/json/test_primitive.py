c33eff617365685f63c9bee5810b77b194dfe8e7
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-03-07 12:11:35 +0000

    Corrected test names.

--- hgijson/tests/json/test_primitive.py
+++ hgijson/tests/json/test_primitive.py
@@ -11,13 +11,13 @@ class TestStrJSONEncoder(unittest.TestCase):
     """
     Tests for `StrJSONEncoder`.
     """
-    def test_decode_with_str(self):
+    def test_default_with_str(self):
         self.assertEqual(StrJSONEncoder().default("123"), "123")
 
-    def test_decode_with_int(self):
+    def test_default_with_int(self):
         self.assertEqual(StrJSONEncoder().default(123), "123")
 
-    def test_decode_with_float(self):
+    def test_default_with_float(self):
         self.assertEqual(StrJSONEncoder().default(12.3), "12.3")
 
 
@@ -25,10 +25,10 @@ class TestStrJSONDecoder(unittest.TestCase):
     """
     Tests for `TestStrJSONDecoder`.
     """
-    def test_default_with_string(self):
+    def test_decode_with_string(self):
         self.assertEqual(StrJSONDecoder().decode("123"), "123")
 
-    def test_with_json_loads_and_string(self):
+    def test_with_json_loads(self):
         self.assertEqual(json.loads("123", cls=StrJSONDecoder), "123")
 
 
@@ -36,13 +36,13 @@ class TestIntJSONEncoder(unittest.TestCase):
     """
     Tests for `IntJSONEncoder`.
     """
-    def test_decode_with_str(self):
+    def test_default_with_str(self):
         self.assertEqual(IntJSONEncoder().default("123"), 123)
 
-    def test_decode_with_int(self):
+    def test_default_with_int(self):
         self.assertEqual(IntJSONEncoder().default(123), 123)
 
-    def test_decode_with_float(self):
+    def test_default_with_float(self):
         self.assertEqual(IntJSONEncoder().default(12.3), 12)
 
 
@@ -50,7 +50,7 @@ class TestIntJSONDecoder(unittest.TestCase):
     """
     Tests for `IntJSONDecoder`.
     """
-    def test_default_with_int_as_string(self):
+    def test_decode_with_int_as_string(self):
         self.assertEqual(IntJSONDecoder().decode("123"), 123)
 
     def test_with_json_loads_and_int_as_string(self):
@@ -61,13 +61,13 @@ class TestFloatJSONEncoder(unittest.TestCase):
     """
     Tests for `FloatJSONEncoder`.
     """
-    def test_decode_with_str(self):
+    def test_default_with_str(self):
         self.assertEqual(FloatJSONEncoder().default("12.3"), 12.3)
 
-    def test_decode_with_int(self):
+    def test_default_with_int(self):
         self.assertEqual(FloatJSONEncoder().default(123), 123.0)
 
-    def test_decode_with_float(self):
+    def test_default_with_float(self):
         self.assertEqual(FloatJSONEncoder().default(12.3), 12.3)
 
 
@@ -75,7 +75,7 @@ class TestFloatJSONDecoder(unittest.TestCase):
     """
     Tests for `FloatJSONDecoder`.
     """
-    def test_default_with_float_as_string(self):
+    def test_decode_with_float_as_string(self):
         self.assertEqual(FloatJSONDecoder().decode("12.3"), 12.3)
 
     def test_with_json_loads_and_int_as_string(self):
