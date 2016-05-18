5068d6623131f6abf0c28ce685de3211bbb45ceb
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-05-18 16:56:46 +0100

    Allowed imports via `__init__.py`.

---
+++ hgijson/__init__.py
@@ -0,0 +1,10 @@
+from hgijson.json.primitive import IntJSONEncoder, StrJSONEncoder, IntJSONDecoder, FloatJSONEncoder, FloatJSONDecoder, \
+    DatetimeISOFormatJSONEncoder, DatetimeISOFormatJSONDecoder, DatetimeEpochJSONEncoder, DatetimeEpochJSONDecoder, \
+    SetJSONEncoder, SetJSONDecoder, ItemType, StrJSONDecoder
+
+from hgijson.json.builders import MappingJSONDecoderClassBuilder, SetJSONEncoderClassBuilder, \
+    SetJSONDecoderClassBuilder, MappingJSONEncoderClassBuilder
+
+from hgijson.json.models import JsonPropertyMapping
+
+
